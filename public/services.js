f1ee6502c690099ed43305ccd729705bef360d9c
Aaron <aaronakinwande@gmail.com>
2025-04-21 17:13:35 +0100

    images revamp

--- public/services.js
+++ public/services.js
@@ -7,20 +7,20 @@ const services = [
         name: "HTML5",
         description:
           "I use semantic HTML5 to structure modern, accessible web pages.",
-        image: "../src/assets/icons/code.png",
+        image: "/icons/code.png",
       },
       {
         id: 8,
         name: "TAILWIND CSS",
         description: "I build responsive and scalable UIs with Tailwind CSS.",
-        image: "../src/assets/icons/code.png",
+        image: "/icons/code.png",
       },
       {
         id: 9,
         name: "JAVASCRIPT",
         description:
           "I write clean and maintainable JavaScript code for both frontend and backend applications.",
-        image: "../src/assets/icons/javascript.png",
+        image: "/icons/javascript.png",
       },
     ],
   },
@@ -32,28 +32,28 @@ const services = [
         name: "REACT.JS",
         description:
           "I develop dynamic single-page applications using React.js.",
-        image: "../src/assets/icons/code.png",
+        image: "/icons/code.png",
       },
       {
         id: 19,
         name: "REDUX & REACT-REDUX",
         description:
           "I manage complex state logic using Redux and React-Redux.",
-        image: "../src/assets/icons/code.png",
+        image: "/icons/code.png",
       },
       {
         id: 11,
         name: "NEXT.JS",
         description:
           "I build fast and optimized React-based apps with Next.js.",
-        image: "../src/assets/icons/code.png",
+        image: "/icons/code.png",
       },
       {
         id: 13,
         name: "TYPESCRIPT",
         description:
           "I use TypeScript to write type-safe and scalable JavaScript applications.",
-        image: "../src/assets/icons/javascript.png",
+        image: "/icons/javascript.png",
       },
     ],
   },
@@ -65,7 +65,7 @@ const services = [
         name: "NODE.JS & EXPRESS.JS ",
         description:
           "I have a solid understanding of Node.js for backend and Express.js for server-side logic and APIs .",
-        image: "../src/assets/icons/javascript.png",
+        image: "/icons/javascript.png",
       },
     ],
   },
@@ -77,21 +77,21 @@ const services = [
         name: "VERSION CONTROL",
         description:
           "I use version control systems such as Git and GitHub to manage code efficiently.",
-        image: "../src/assets/icons/git-repo.png",
+        image: "/icons/git-repo.png",
       },
       {
         id: 14,
         name: "GIT & GITHUB",
         description:
           "I manage projects and collaborate with teams using Git and GitHub.",
-        image: "../src/assets/icons/git-repo.png",
+        image: "/icons/git-repo.png",
       },
       {
         id: 18,
         name: "SCRUM",
         description:
           "I work well in agile teams, following Scrum methodologies for effective delivery.",
-        image: "../src/assets/icons/web-scraping.png",
+        image: "/icons/web-scraping.png",
       },
     ],
   },
@@ -103,7 +103,7 @@ const services = [
         name: "MICROSOFT OFFICE",
         description:
           "I am proficient in Microsoft Word, Excel, PowerPoint, and Access.",
-        image: "../src/assets/icons/web-scraping.png",
+        image: "/icons/web-scraping.png",
       },
     ],
   }
